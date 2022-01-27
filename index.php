92a571eaa5d5c79bc00d815f798b9db365ad4f95
Droses2 <98118910+Droses2@users.noreply.github.com>
2022-01-27 15:44:55 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>Cursos Basicos</title>
+    <meta charset="utf-8">
+    <style>
+      
+    </style>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <br /><br /> <br /> <br /> <br /> 
+  </head>
+  <body>
+    <header>
+     <div>
+        <img src="imagenes/imagen.jpg" width="50" alt="Logotipo">
+     </div>  
+     <nav>
+       <ul>
+         <li><a href="#">Inicio</a></li>
+         <li><a href="#">Categoria</a></li>
+         <li><a href="#">Categoria 2</a></li>
+       </ul>
+     </nav>   
+     </header> 
+
+    <section class="wrapper">
+        <section class="main">
+          <article>
+          <h2 style="font-size:190%;" style="font-family:georgia;" style="font-size:38px;">Cursos de programas basicos</h2>
+          </article>
+        </section>
+        <aside></aside>
+
+    <br>
+    <h3 style="font-size:185%;"  style="font-family:georgia;" style="font-size:25px;">Cursos para dar tus primeros pasos</h3>
+    <hr>
+  </body>
+  
+</html>
